a5804372dc3b64a45942a360e922292f0db81bbc
Bartosz Sekula <Bartosz.Sekula@episerver.com>
2020-08-04 15:14:12 +0200

    Turn on all project options by default

--- src/EPiServer.Labs.ProjectEnhancements/ProjectOptions.cs
+++ src/EPiServer.Labs.ProjectEnhancements/ProjectOptions.cs
@@ -6,7 +6,7 @@ namespace EPiServer.Labs.ProjectEnhancements
     public class ProjectOptions
     {
         /// <summary>
-        /// When true, then page tree will display indicator 
+        /// When true, then page tree will display indicator
         /// for pages that are part of current project
         /// </summary>
         public bool ShowPageTreeIndicator { get; set; } = true;
@@ -14,16 +14,16 @@ namespace EPiServer.Labs.ProjectEnhancements
         /// <summary>
         /// When true, then project description is available
         /// </summary>
-        public bool ShowDescription { get; set; }
+        public bool ShowDescription { get; set; } = true;
 
         /// <summary>
         /// When true, then project categories are available
         /// </summary>
-        public bool ShowCategories { get; set; }
+        public bool ShowCategories { get; set; } = true;
 
         /// <summary>
         /// When true, then notification tooltip with project information is available
         /// </summary>
-        public bool ShowNotificationTooltip { get; set; }
+        public bool ShowNotificationTooltip { get; set; } = true;
     }
 }
