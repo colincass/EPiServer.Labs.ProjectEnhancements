ccfcb285cc0e80e49ac7226b6010ca8238966320
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-08-06 18:42:05 +0200

    Add hash prefix for colors
    Feature: CMS-16113

--- src/EPiServer.Labs.ProjectEnhancements/ProjectCategory/ProjectCategoryColor.cs
+++ src/EPiServer.Labs.ProjectEnhancements/ProjectCategory/ProjectCategoryColor.cs
@@ -2,13 +2,13 @@
 {
     public static class ProjectCategoryColor
     {
-        public const string Gray = "DBDBDB";
-        public const string Teal = "A1E5F3";
-        public const string Yellow = "F3EF87";
-        public const string Pink = "F3A1C9";
-        public const string Purple = "DCB3F5";
-        public const string Green = "C5F3A1";
-        public const string Brown = "EEBD9A";
-        public const string None = "FFFFFF";
+        public const string Gray = "#DBDBDB";
+        public const string Teal = "#A1E5F3";
+        public const string Yellow = "#F3EF87";
+        public const string Pink = "#F3A1C9";
+        public const string Purple = "#DCB3F5";
+        public const string Green = "#C5F3A1";
+        public const string Brown = "#EEBD9A";
+        public const string None = "#FFFFFF";
     }
 }
