40f49b941a50f7ac94b3b22dfb4e5227b7a54893
Bartosz Sekula <Bartosz.Sekula@episerver.com>
2020-08-07 10:16:10 +0200

    lint script

--- build.cmd
+++ build.cmd
@@ -1,10 +1,5 @@
 @ECHO OFF
 SETLOCAL
 
-SET PATH=.\.ci\tools\;.\build\tools\;%PATH%
-
-REM Lint the JavaScript files.
-CALL yarn lint:js
-IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
-
+CALL lint.cmd
 msbuild /p:Configuration=Release
