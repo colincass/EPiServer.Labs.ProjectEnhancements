b012d548b4f878bc0cf16cc1f7460ed8b9320c8a
Bartosz Sekula <Bartosz.Sekula@episerver.com>
2020-08-11 13:41:23 +0200

    boyscout: remove unused usings

--- src/EPiServer.Labs.ProjectEnhancements/ExtendedProjectViewModel.cs
+++ src/EPiServer.Labs.ProjectEnhancements/ExtendedProjectViewModel.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using EPiServer.Approvals;
-using EPiServer.Cms.Shell.UI.Notifications.Internal;
 using EPiServer.Web;
 
 namespace EPiServer.Labs.ProjectEnhancements
